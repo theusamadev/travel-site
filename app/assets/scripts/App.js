64f544ebd66c8935e4f262d48c09e12667a3564a
Usamaumar118 <46302314+Usamaumar118@users.noreply.github.com>
2020-02-28 11:49:32 +0500

    Completed the lightbox and splitted the code

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,18 +1,28 @@
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/revealOnScroll'
 import StickyHeader from './modules/StickyHeader'
-
-
 import('../styles/styles.css');
 // import('./modules/MobileMenu');
-
-
 
 
 let stickyHeader = new StickyHeader();
 let revealOnScroll  = new RevealOnScroll();
 let mobileMenu = new MobileMenu();
+let modal
 
+document.querySelectorAll('.open-modal').forEach( el => {
+    el.addEventListener('click', e => {
+        e.preventDefault()
+        if(typeof modal == 'undefined'){
+            import(/* webpackChunkName: "modal" */ './modules/Modal').then(x => {
+                modal = new x.default()
+                setTimeout(() => modal.openTheModal(), 20)
+            }).catch(() => console.log('There is a problem'))
+        }else{
+            modal.openTheModal()
+        }
+    })
+})
 
 
 if(module.hot){
